12443e7e3a12079ec19bb17ac3733e98ab9571dc
Leonardo Mariotto <leonardo_mariotto@yahoo.com.br>
2018-05-21 14:57:48 -0300

    Add files via upload

---
+++ Main.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+	char palavra[50];
+	int i;
+
+	while(scanf("%s", &palavra) != EOF)
+	{
+	i++;
+	}
+
+	printf("%i\n", i);
+	
+	return 0;
+}
---
+++ ST.h
@@ -0,0 +1,5 @@
+struct tree
+{
+int temp
+int vrau
+};
