4b2d21e307ba6661157b7f836343db4e7ae11ca6
matheusvalbert <33065794+matheusvalbert@users.noreply.github.com>
2018-05-27 21:04:50 -0300

    Update ST.h

--- ST.h
+++ ST.h
@@ -1,7 +1,20 @@
-typedef struct tTeste teste;
+typedef struct tArvore arvore;
+typedef struct tLista lista;
 
-struct tTeste
+struct tArvore
 {
-	int temp;
-	int vrau;
+	char palavra[50];
+	int frequencia;
+	struct tArvore *dir, *esq;
 };
+
+struct tLista {
+	char palavra[50];
+	int frequencia;
+	struct tLista *prox;
+};
+
+int Inserir(char palavra[], arvore **raiz);
+void Imprimir(arvore *raiz, lista **inicio);
+void ordenaPorValor(arvore *raiz, lista **inicio);
+void print(lista *inicio, int exibir);
